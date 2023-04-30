6f1457e8a110c8c428c22e6d57ee519dd190e6f4
unknown <you@example.com>
2023-04-30 20:24:58 +0200

    fixed nerdfonts.sh errors

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -1,5 +1,5 @@
 [ ! -d "$HOME/.fonts" ] && mkdir "$HOME/.fonts"
-wget --output-document '$HOME/.fonts/MesloLGS NF Regular.ttf' {{https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf}}
-wget --output-document '$HOME/.fonts/MesloLGS NF Bold.ttf' {{https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf}}
-wget --output-document '$HOME/.fonts/MesloLGS NF Italic.ttf' {{https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf}}
-wget --output-document '$HOME/.fonts/MesloLGS NF Bold Italic.ttf' {{https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf}}
+wget -P '$HOME/.fonts' https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget -P '$HOME/.fonts' https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget -P '$HOME/.fonts' https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget -P '$HOME/.fonts' https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
