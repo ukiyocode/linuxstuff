0269956cfb94053da711730694e93aa4f4e226b7
ukiyocode <77393110+ukiyocode@users.noreply.github.com>
2024-09-16 17:19:52 +0200

    -nanorc -nerdfints +micro conf

--- nerdfonts.sh
+++
@@ -1,5 +0,0 @@
-[ ! -d "$HOME/.fonts" ] && mkdir "$HOME/.fonts"
-wget -P "$HOME/.fonts" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget -P "$HOME/.fonts" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget -P "$HOME/.fonts" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget -P "$HOME/.fonts" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
